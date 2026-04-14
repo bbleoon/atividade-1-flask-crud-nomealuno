49487df02bd13d7450ab02c01921c3bea1d8c7f2
bbleoon <lnunes.ber@gmail.com>
2026-04-14 09:33:59 -0300

    Implementação do método atualizar e método excluir, criação de uma interface separada para o método atualizar

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ def adicionar_filme():
     nome = request.form["nome"]
     sinopse = request.form["sinopse"]
     elenco = request.form["elenco"]
-    #dtlancamento = date(request.form["dtlancamento"])
     dtlancamento = (request.form["dtlancamento"])
 
     novo_filme = Filme(nome,sinopse,elenco,dtlancamento)
@@ -27,5 +26,28 @@ def adicionar_filme():
 
     return redirect("/")
 
+@app.route("/remover/<int:id>")
+def remover_filme(id):
+    global filmes
+    filmes = [filme for filme in filmes if filme.id != id]
+    return redirect("/")
+
+@app.route("/editar/<int:id>")
+def editar_filme(id):
+    filme = next((filme for filme in filmes if filme.id == id), None)
+    if filme:
+        return render_template("editar.html",filme=filme)
+    return redirect("/")
+
+@app.route("/atualizar/<int:id>", methods=["POST"])
+def atualizar_filme(id):
+    filme = next((filme for filme in filmes if filme.id == id), None)
+    if filme:
+        filme.nome = request.form["nome"]
+        filme.sinopse = request.form["sinopse"]
+        filme.elenco = request.form["elenco"]
+        filme.dtlancamento = request.form ["dtlancamento"]
+    return redirect("/")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="191.36.57.66")
